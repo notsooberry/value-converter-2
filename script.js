38dee06e1aa3acb0f64fe7b893bd53c4e1a37a04
notsooberry <suvorova.200340@gmail.com>
2022-10-13 22:33:55 +0300

    Add MetersToCentimeters/MetersToKilometers

---
+++ script.js
@@ -0,0 +1,11 @@
+function MetersToCentimeters(num){
+    num = num * 100;
+    return num;
+}
+alert("Метры в сантиметры: " + MetersToCentimeters(5));
+
+function MetersToKilometers(num){
+    num = num/1000;
+    return num;
+}
+alert("Метры в километры: " + MetersToKilometers(500));
