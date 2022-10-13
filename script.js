bdf3011358cd43a4f132558ec27dff55a9354c8e
notsooberry <suvorova.200340@gmail.com>
2022-10-13 22:36:42 +0300

    Add GramsToLiters/LitersToGallons

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ function MetersToKilometers(num){
     return num;
 }
 alert("Метры в километры: " + MetersToKilometers(500));
+
+function GramsToLiters(num){
+    num = num/1000;
+    return num;
+}
+alert("Граммы в литры:  "+ GramsToLiters(50000));
+
+function LitersToGallons(num){
+    num = num * 0.219969;
+    return num;
+}
+alert("Литры в галлоны:  "+ LitersToGallons(100));
